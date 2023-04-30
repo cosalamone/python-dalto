82e6c0c0870c2fdd8ae4701374669061d5656b7a
cosalamone <cosalamone@gmail.com>
2023-04-30 15:04:38 -0300

    operadores aritmeticos y de comparacion

---
+++ operadores/operadores_aritmeticos.py
@@ -0,0 +1,27 @@
+# sume y resta: + y - 
+suma = 12 +5 
+resta= 12-5
+print(suma)
+print(resta)
+
+
+#multiplicacion y division: * y /
+muplip = 5*5
+divsion= 6/2  # la division devuelve un dato float(flotante) 
+print(muplip)
+print(divsion)
+
+
+#potenciacion (exponente): **
+exponente= 12**5 #12 elevado a la 5ta
+print(exponente)
+
+
+#division baja: //
+division_baja= 12//5
+print(division_baja)
+
+
+#resto o módulo
+resto= 12%5
+print(resto)
---
+++ operadores/operadores_de_comparacion.py
@@ -0,0 +1,20 @@
+#DEVUELVEN BOOLEANOS: True o False
+
+# igual que ==
+
+# distinto que !=
+
+# menor que <
+
+# mayor que >
+
+# menor o igual que <=
+
+# mayor o igual que >=
+
+a = 5
+b= 10
+c= 20
+
+comparacion = a + b == c
+print(comparacion) # False
