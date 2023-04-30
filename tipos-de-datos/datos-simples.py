f42f786c4ee40a81f7f24956b7e2e83c843741a1
cosalamone <cosalamone@gmail.com>
2023-04-30 14:16:37 -0300

    datos-simples-python

---
+++ tipos-de-datos/datos-simples.py
@@ -0,0 +1,19 @@
+'textos = string'
+
+"cadena de texto"
+
+"""forma valida de texto
+para viarias lineas: 
+puedo escribir en varias lineas
+--> permite MULTIPLES LINEAS <--
+"""
+
+'numeros hay 2 tipos: enteros/int(1) y flotantes/float(1.5)'
+40 
+40.2
+
+'boolean: True, False --> Se escribe inicial con mauys'
+
+False
+True
+
