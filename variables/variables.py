3d6fcc0cb55f58a764689aa03b9e28e6c0ddcfb5
cosalamone <cosalamone@gmail.com>
2023-04-30 14:32:53 -0300

    variables

---
+++ variables/variables.py
@@ -0,0 +1,26 @@
+#VARIABLES
+'las variables son modificables, se pueden redefinir'
+numero = 10
+numero += 2
+print(numero)
+
+
+#FORMA + DE CONCATENAR
+nombre = "lucas"
+nombre = "mario"
+bienvenida = "hola " + nombre + " como estas?"
+print(bienvenida)
+
+
+#FSTRING = FORMA DE CONTACTERNAR 
+name = 5
+bienveni2 = f"hola {name} ¿cómo va?"
+del bienveni2
+'del: es un operador para borrar datos'
+
+
+#OPERADORES DE PERTENENCIA: INDICAN SI ALGO ESTÁ O NO EN UNA VARIABLE, RETORNA BOOLEANO 
+print("hola" in bienvenida)
+
+#EN PYTHON SE USA SNAKE_CASE
+nombre_completo = "lucas dalto"
